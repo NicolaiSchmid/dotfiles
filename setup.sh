e26ae641c99eb07ec6eab5aab027fe281f0b0108
Nicolai Schmid <nicolai@schmid.uno>
2018-10-05 19:01:09 +0200

    feat: fixed gpg settings and added note how to fetch the public key from smartcard

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 sudo pacman -Sy
-sudo pacman -Sy base-devel yaourt snapd pacaur
-
-sudo systemctl enable --now snapd.socket
-
-sudo ln -s /var/lib/snapd/snap /snap
-
-sudo snap install spotify insomnia vlc
-sudo snap install vscode --classic
-sudo snap install slack --classic
+sudo pacman -Sy base-devel yaourt pacaur curl
 
 echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="users", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="f1d0"' | sudo tee /etc/udev/rules.d/10-security-key.rules
 
-pacaur -Sy yarn docker git google-chrome yubikey-manager yubikey-personalization libu2f-host yubikey-personalization-gui-git vivaldi kubectl minikube zsh
+pacaur -Sy yarn docker git google-chrome yubikey-manager yubikey-personalization libu2f-host 
+yubikey-personalization-gui-git vivaldi kubectl minikube zsh visual-studio-code-bin spotify 
+slack-desktop insomnia
 
 sudo systemctl enable pcscd.service
 
 bash ~/git/personal/dotfiles/symlink-setup.sh
 
-# Import public key of the yubikey-key
-gpg --import < curl https://keybase.io/nicolaischmid/key.asc
-
 sudo chsh -s $(which zsh) $USER
+
+# GPG2 settings
+cat >> ~/.gnupg/gpg-agent.conf << EOF
+default-cache-ttl 600
+max-cache-ttl 7200
+enable-ssh-support
+write-env-file
+EOF
 
 # NVM
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
-
+echo "Run gpg2 --card-edit"
+echo "Then in the interactive terminal run 'fetch'"
