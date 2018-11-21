f2a3e128ead142546f44b2ea05f7dd12632393a1
Nicolai Schmid <nicolai@schmid.uno>
2018-11-21 21:11:28 +0100

    feat: add dnsutils

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Sy
-sudo pacman -Sy base-devel yaourt pacaur curl
+sudo pacman -Sy base-devel yaourt pacaur curl dnsutils
 
 echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="users", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="f1d0"' | sudo tee /etc/udev/rules.d/10-security-key.rules
 
