a8f73d320ca83e05f9b5af68cf918c6b76fc8013
Nicolai Schmid <nicolai@schmid.uno>
2019-03-03 18:33:13 +0100

    feat: add kubernetes-helm

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="users", ATTRS{
 
 pacaur -Sy yarn docker git google-chrome yubikey-manager yubikey-personalization libu2f-host 
 yubikey-personalization-gui-git vivaldi kubectl minikube zsh visual-studio-code-bin spotify 
-slack-desktop insomnia telepresence-git antigen-git
+slack-desktop insomnia telepresence-git antigen-git kubernetes-helm-bin
 
 sudo systemctl enable pcscd.service
 
